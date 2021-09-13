83eaad38d2e2d19d03fb67262c6e4d06f76bc107
benf3632 <benf3632@gmail.com>
2021-09-13 04:40:02 +0300

    Configured project to run and build for electron

---
+++ electron/main.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+const url = require("url");
+
+let mainWindow;
+
+function createWindow() {
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, "../index.html"),
+      protocol: "file:",
+      slashes: true,
+    });
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+  mainWindow.loadURL(startUrl);
+  mainWindow.removeMenu();
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform != "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
