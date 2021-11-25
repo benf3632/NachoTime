c0989c0bb64b4d199341a931218b7265e54bb358
benf3632 <benf3632@gmail.com>
2021-11-26 01:19:57 +0200

    Added movie cover and details

--- src/screens/MovieScreen.js
+++ src/screens/MovieScreen.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import { MdOutlineArrowBackIos } from "react-icons/md";
 import { useEffect, useState } from "react";
+import IMDBLogo from "../assets/IMDB_Logo.svg";
 
 const fetchMovieDetails = async (movieId) => {
 	const response = await fetch(
@@ -15,9 +16,10 @@ const MovieScreen = () => {
 	let { movieID } = useParams();
 	const [movieDetails, setMovieDetails] = useState(null);
 
-	useEffect( () => {
+	useEffect(() => {
 		const fetchMovie = async () => {
 			const movie = await fetchMovieDetails(movieID);
+			console.log(movie);
 			setMovieDetails(movie);
 		};
 		fetchMovie();
@@ -27,31 +29,104 @@ const MovieScreen = () => {
 		<div
 			style={{
 				display: "flex",
-				justifyContent: "center",
-				alignItems: "center",
 				width: "100%",
-				color: "white",
 				flexDirection: "column",
+				padding: "40px",
 			}}
 		>
-			{movieID}<br></br>
-			{movieDetails && movieDetails.title}	
-			<Link
+			<div
 				style={{
-					height: "25px",
-					width: "25px",
-					textAlign: "center",
+					display: "flex",
+					width: "100%",
+					flexDirection: "row",
 				}}
-				to="/"
 			>
-				<MdOutlineArrowBackIos
+				<Link style={{}} to="/">
+					<MdOutlineArrowBackIos
+						style={{
+							width: "100%",
+							height: "100%",
+							color: "white",
+						}}
+					/>
+				</Link>
+				<span
 					style={{
-						width: "100%",
-						height: "100%",
-						color: "black",
+						flex: 1,
+						justifyContent: "center",
+						display: "flex",
+						color: "white",
+						fontWeight: "bold",
+						fontSize: "1.5em",
 					}}
-				/>
-			</Link>
+				>
+					{movieDetails && movieDetails.title_long}
+				</span>
+			</div>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					width: "100%",
+					paddingTop: "3%",
+				}}
+			>
+				<div
+					style={{
+						width: "25%",
+					}}
+				>
+					<img
+						src={movieDetails && movieDetails.large_cover_image}
+						style={{
+							width: "100%",
+						}}
+					/>
+				</div>
+				<div
+					style={{
+						display: "flex",
+						flex: 2,
+						color: "white",
+						paddingLeft: "2%",
+						paddingRight: "2%",
+						flexDirection: "column",
+						justifyContent: "space-between",
+					}}
+				>
+					<span
+						style={{
+							fontSize: "1em",
+							height: "100%",
+							display: 'flex',
+							justifyContent:'space-between',
+							flexDirection: 'column'
+						}}
+					>
+						<div style={{
+							height: '100%',
+							display: 'flex',
+							flexDirection: 'row',
+							justifyContent: 'space-evenly'
+							}}>
+							{movieDetails && movieDetails.genres.join(" - ")}
+							<img
+								src={IMDBLogo}
+								alt="imdb"
+								style={{
+									width: '10%',
+									display: "inline-block",
+									objectFit: "contain",
+								}}
+							/>
+						</div>
+						{movieDetails && movieDetails.description_full}
+					</span>
+					<div>
+						<span>Test</span>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
