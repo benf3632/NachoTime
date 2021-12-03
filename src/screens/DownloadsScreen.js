0604b14514eee48fde42d11eeead2c52466e9f79
benf3632 <benf3632@gmail.com>
2021-12-03 03:52:27 +0200

    Added listing torrent progress

--- src/screens/DownloadsScreen.js
+++ src/screens/DownloadsScreen.js
@@ -3,6 +3,18 @@ import { useState, useEffect } from "react";
 // eletcron
 const { ipcRenderer } = window.require("electron");
 
+const parseSpeed = (speed) => {
+	let speedNumber = parseInt(speed);
+	if (speedNumber >= 1000 * 1000) {
+		speedNumber = speedNumber / (1000 * 1000);
+		return `${speedNumber} Mb/S`
+	} else if (speedNumber >= 1000) {
+		speedNumber = speedNumber / (1000);
+		return `${speedNumber} Kb/S`
+	}
+	return `${speed} b/s`
+}
+
 const DownloadsScreen = () => {
 	const [progress, setProgress] = useState(null);
 
@@ -14,7 +26,18 @@ const DownloadsScreen = () => {
 		return () => ipcRenderer.removeAllListeners("wt-progress");
 	}, []);
 
-	return <div style={{color: 'white'}}>{progress && progress.progress}</div>;
+	return (
+		<div style={{ color: "white", display: 'grid', justifyContent: 'center', marginRight: '10%' }}>
+			{progress && progress.torrents.map(torrent => {
+				return (
+					<div>
+						<p>{torrent.torrentName}</p>
+						<p>Downloaded: {Math.floor(parseFloat(torrent.progress) * 100)}% {parseSpeed(torrent.downloadSpeed)}</p>
+					</div>
+				)
+			})}
+		</div>
+	);
 };
 
 export default DownloadsScreen;
