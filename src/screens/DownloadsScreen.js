d3fd1bdb375f003d4e46430b3766155168c38003
benf3632 <benf3632@gmail.com>
2021-12-03 05:42:00 +0200

    Styled torrents progress in downloads screen

--- src/screens/DownloadsScreen.js
+++ src/screens/DownloadsScreen.js
@@ -1,4 +1,11 @@
 import { useState, useEffect } from "react";
+import ProgressBar from "@ramonak/react-progress-bar";
+import { IoMdCloudDownload, IoMdCloudUpload } from "react-icons/io/";
+import { AiOutlinePauseCircle, AiOutlinePlayCircle } from "react-icons/ai";
+import { FaTrashAlt } from "react-icons/fa";
+
+// css
+import "./DownloadsScreen.css";
 
 // eletcron
 const { ipcRenderer } = window.require("electron");
@@ -7,13 +14,13 @@ const parseSpeed = (speed) => {
 	let speedNumber = parseInt(speed);
 	if (speedNumber >= 1000 * 1000) {
 		speedNumber = speedNumber / (1000 * 1000);
-		return `${speedNumber} Mb/S`
+		return `${speedNumber} MB/s`;
 	} else if (speedNumber >= 1000) {
-		speedNumber = speedNumber / (1000);
-		return `${speedNumber} Kb/S`
+		speedNumber = speedNumber / 1000;
+		return `${speedNumber} KB/s`;
 	}
-	return `${speed} b/s`
-}
+	return `${speed} B/s`;
+};
 
 const DownloadsScreen = () => {
 	const [progress, setProgress] = useState(null);
@@ -27,15 +34,38 @@ const DownloadsScreen = () => {
 	}, []);
 
 	return (
-		<div style={{ color: "white", display: 'grid', justifyContent: 'center', marginRight: '10%' }}>
-			{progress && progress.torrents.map(torrent => {
-				return (
-					<div>
-						<p>{torrent.torrentName}</p>
-						<p>Downloaded: {Math.floor(parseFloat(torrent.progress) * 100)}% {parseSpeed(torrent.downloadSpeed)}</p>
-					</div>
-				)
-			})}
+		<div className="TorrentsProgressContainer">
+			{progress &&
+				progress.torrents.map((torrent) => {
+					return (
+						<div className="TorrentProgress" key={torrent.infoHash}>
+							<div style={{ width: "100%" }}>
+								<p>{torrent.torrentName}</p>
+								<ProgressBar
+									bgColor="#66bb6a"
+									labelAlignment="center"
+									completed={Math.floor(
+										parseFloat(torrent.progress) * 100
+									)}
+								/>
+								<p className="TorrentProgressInfoConatiner">
+									<div>
+										{<IoMdCloudDownload />}
+										{"    "}
+										{parseSpeed(torrent.downloadSpeed)}{" "}
+										{<IoMdCloudUpload />}
+										{"    "}
+										{parseSpeed(torrent.uploadSpeed)}
+									</div>
+									<div>
+										{<AiOutlinePauseCircle className="ClickableIcon" />}{" "}
+										{<FaTrashAlt className="ClickableIcon" onClick={() => console.log("Deleted Torrent")} />}
+									</div>
+								</p>
+							</div>
+						</div>
+					);
+				})}
 		</div>
 	);
 };
