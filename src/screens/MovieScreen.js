d5e3f29ff1b91fcce2b1950ef9ec3f778935a8f4
benf3632 <benf3632@gmail.com>
2021-12-30 12:00:58 +0200

    added watch method selection

--- src/screens/MovieScreen.js
+++ src/screens/MovieScreen.js
@@ -3,6 +3,7 @@ import { useHistory, useParams } from "react-router";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { MdOutlineArrowBackIos } from "react-icons/md";
+import { SiVlcmediaplayer } from "react-icons/si";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import Select from "@mui/material/Select";
 import { MenuItem } from "@mui/material";
@@ -16,217 +17,247 @@ import MaterialButton from "../components/MaterialButton";
 
 // assets
 // import IMDBLogo from "../assets/IMDB_Logo.svg";
+import Nacho from "../assets/nacho.png";
 
 // css
 import "./MovieScreen.css";
+import { methodOf } from "lodash";
 
 // electron
 const { ipcRenderer } = window.require("electron");
 
 // consts
 const trackers =
-	"tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://p4p.arenabg.ch:1337&tr=udp://tracker.internetwarriors.net:1337";
+  "tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://p4p.arenabg.ch:1337&tr=udp://tracker.internetwarriors.net:1337";
 
 const fetchMovieDetails = async (movieId) => {
-	const response = await fetch(
-		`https://yts.mx/api/v2/movie_details.json?movie_id=${movieId}`
-	);
-	const json = await response.json();
-	return json.data.movie;
+  const response = await fetch(
+    `https://yts.mx/api/v2/movie_details.json?movie_id=${movieId}`
+  );
+  const json = await response.json();
+  return json.data.movie;
 };
 
 const MovieScreen = ({ torrents, startNewTorrent, addMessage }) => {
-	let { movieID } = useParams();
-	let history = useHistory();
-	const [movieDetails, setMovieDetails] = useState(null);
-	const [coverLoading, setCoverLoading] = useState(true);
-	const [movieQuality, setMovieQuality] = useState(null);
-	const [selectedQuality, setSelectedQuality] = useState(null);
+  let { movieID } = useParams();
+  let history = useHistory();
+  const [movieDetails, setMovieDetails] = useState(null);
+  const [coverLoading, setCoverLoading] = useState(true);
+  const [movieQuality, setMovieQuality] = useState(null);
+  const [selectedQuality, setSelectedQuality] = useState(null);
+  const [selectedWatchMethod, setSelectedWatchMethod] = useState(null);
+  const [watchMethodOptions, setWatchMethodOptions] = useState(null);
 
-	const handleCoverLoaded = () => {
-		setCoverLoading(false);
-	};
+  const handleCoverLoaded = () => {
+    setCoverLoading(false);
+  };
 
-	const startTorrenting = () => {
-		const torrentToDownload = getBestSeedMovie();
-		const torrentId = `magnet:?xt=urn:btih:${torrentToDownload.hash}&${trackers}`;
-		startNewTorrent(
-			`${torrentToDownload.title_long} (${torrentToDownload.quality})`,
-			torrentId,
-			torrentToDownload.hash.toLowerCase(),
-			"/home/cookies/webtorrent"
-		);
-	};
+  const startTorrenting = () => {
+    const torrentToDownload = getBestSeedMovie();
+    const torrentId = `magnet:?xt=urn:btih:${torrentToDownload.hash}&${trackers}`;
+    startNewTorrent(
+      `${torrentToDownload.title_long} (${torrentToDownload.quality})`,
+      torrentId,
+      torrentToDownload.hash.toLowerCase(),
+      "/home/cookies/webtorrent"
+    );
+  };
 
-	const handleDownloadMovieClick = () => {
-		const torrentToDownload = getBestSeedMovie();
-		const torrentExists = torrents.some(
-			(torrent) =>
-				torrent.infoHash === torrentToDownload.hash.toLowerCase()
-		);
-		if (torrentExists && !torrentExists.stopped) {
-			addMessage("The Movie is already in downloads!", "error");
-			return;
-		}
-		startTorrenting();
-		addMessage(
-			`Started downloading: ${movieDetails.title_long}!`,
-			"success"
-		);
-	};
+  const handleDownloadMovieClick = () => {
+    const torrentToDownload = getBestSeedMovie();
+    const torrentExists = torrents.some(
+      (torrent) => torrent.infoHash === torrentToDownload.hash.toLowerCase()
+    );
+    if (torrentExists && !torrentExists.stopped) {
+      addMessage("The Movie is already in downloads!", "error");
+      return;
+    }
+    startTorrenting();
+    addMessage(`Started downloading: ${movieDetails.title_long}!`, "success");
+  };
 
-	const getBestSeedMovie = () => {
-		console.log(selectedQuality);
-		const qualityTorrents = movieDetails.torrents.filter(
-			(torrent) => torrent.quality === selectedQuality
-		);
+  const getBestSeedMovie = () => {
+    console.log(selectedQuality);
+    const qualityTorrents = movieDetails.torrents.filter(
+      (torrent) => torrent.quality === selectedQuality
+    );
 
-		const bestSeedMovie = qualityTorrents.reduce(
-			(prev, current) => (current.seed > prev.seed ? current : prev),
-			qualityTorrents[0]
-		);
-		return bestSeedMovie;
-	};
+    const bestSeedMovie = qualityTorrents.reduce(
+      (prev, current) => (current.seed > prev.seed ? current : prev),
+      qualityTorrents[0]
+    );
+    return bestSeedMovie;
+  };
 
-	const handleWatchMovieClick = () => {
-		const torrentToDownload = getBestSeedMovie();
-		const torrentExists = torrents.some(
-			(torrent) =>
-				torrent.infoHash === torrentToDownload.hash.toLowerCase()
-		);
-		if (!torrentExists || torrentExists.stopped) {
-			startTorrenting();
-			addMessage(
-				`Started downloading: ${movieDetails.title_long}!`,
-				"success"
-			);
-		}
-		history.push({
-			pathname: `/buffer/${torrentToDownload.hash.toLowerCase()}`,
-			state: {
-				bufferLoadedCallback: watchWithWebPlayer,
-			},
-		});
-	};
+  const handleWatchMovieClick = () => {
+    const torrentToDownload = getBestSeedMovie();
+    const torrentExists = torrents.some(
+      (torrent) => torrent.infoHash === torrentToDownload.hash.toLowerCase()
+    );
+    if (!torrentExists || torrentExists.stopped) {
+      startTorrenting();
+      addMessage(`Started downloading: ${movieDetails.title_long}!`, "success");
+    }
+    history.push({
+      pathname: `/buffer/${torrentToDownload.hash.toLowerCase()}`,
+      state: {
+        bufferLoadedCallback: selectedWatchMethod.method,
+      },
+    });
+  };
 
-	const watchWithVLC = (torrent) => {
-		if (!torrent) {
-			addMessage("Torrent doesn't exists!", "error");
-		}
+  const watchWithVLC = useCallback((torrent) => {
+    if (!torrent) {
+      addMessage("Torrent doesn't exists!", "error");
+    }
 
-		ipcRenderer.send("open-vlc", "vlc", torrent.file.path);
-	};
+    ipcRenderer.send("open-vlc", "vlc", torrent.file.path);
+  });
 
-	const watchWithWebPlayer = (torrent) => {
-		history.replace(`/player/${encodeURIComponent(torrent.file.path)}`);
-	};
+  const watchWithWebPlayer = useCallback((torrent) => {
+    history.replace(`/player/${encodeURIComponent(torrent.file.path)}`);
+  });
 
-	const handleQualitySelectionChange = (event) => {
-		setSelectedQuality(event.target.value);
-	};
+  const handleQualitySelectionChange = (event) => {
+    setSelectedQuality(event.target.value);
+  };
 
-	useEffect(() => {
-		const fetchMovie = async () => {
-			const movie = await fetchMovieDetails(movieID);
+  const handleSelectWatchMethod = (event) => {
+    setSelectedWatchMethod(
+      watchMethodOptions.find((method) => method.value === event.target.value)
+    );
+  };
 
-			// get available qualities
-			const movieQualitySet = new Set();
-			movie.torrents.forEach((torrent) => {
-				movieQualitySet.add(torrent.quality);
-			});
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const movie = await fetchMovieDetails(movieID);
 
-			const movieQualityOptions = Array.from(movieQualitySet).map(
-				(quality) => ({ value: quality, label: quality })
-			);
-			setMovieQuality(movieQualityOptions);
-			setSelectedQuality(movieQualityOptions[0].value);
+      // get available qualities
+      const movieQualitySet = new Set();
+      movie.torrents.forEach((torrent) => {
+        movieQualitySet.add(torrent.quality);
+      });
 
-			console.log(movie);
-			setMovieDetails(movie);
-		};
-		fetchMovie();
-	}, [movieID]);
+      const movieQualityOptions = Array.from(movieQualitySet).map(
+        (quality) => ({ value: quality, label: quality })
+      );
+      setMovieQuality(movieQualityOptions);
+      setSelectedQuality(movieQualityOptions[0].value);
 
-	return (
-		<div className="MovieScreenContainer">
-			<Link to="/" className="MovieScreenBackIconContainer">
-				<MdOutlineArrowBackIos className="MovieScreenBackIcon" />
-			</Link>
-			<div className="MovieScreenContentContainer">
-				<div className="MovieScreenMovieCoverContainer">
-					{coverLoading && (
-						<SkeletonTheme
-							className="MovieScreenMovieCover"
-							highlightColor="#444"
-							color="#202020"
-						>
-							<Skeleton
-								width={300}
-								height={300}
-								className="MovieScreenMovieCover"
-							/>
-						</SkeletonTheme>
-					)}
-					<img
-						className="MovieScreenMovieCover"
-						style={coverLoading ? { display: "none" } : {}}
-						onLoad={handleCoverLoaded}
-						src={movieDetails && movieDetails.large_cover_image}
-					/>
-				</div>
-				<div className="MovieScreenMovieDetailsContainer">
-					<div>
-						<p className="MovieScreenTitle">
-							{movieDetails && movieDetails.title_long}
-						</p>
-						<p>{movieDetails && movieDetails.genres.join(" - ")}</p>
-						<p>{movieDetails && movieDetails.description_full}</p>
-					</div>
-					<div style={{}}>
-						<div
-							style={{
-								width: "120px",
-								paddingBottom: "1%",
-							}}
-						>
+      console.log(movie);
+      setMovieDetails(movie);
+    };
+    fetchMovie();
+    const watchMethods = [
+      {
+        value: "WebPlayer",
+        label: <img src={Nacho} style={{ width: "20px", height: "20px" }} />,
+        method: watchWithWebPlayer,
+      },
+      { value: "VLC", label: <SiVlcmediaplayer />, method: watchWithVLC },
+    ];
+    setWatchMethodOptions(watchMethods);
+    setSelectedWatchMethod(watchMethods[0]);
+  }, [movieID]);
 
-							<Select
-								className="QualitySelect"
-								id="quality"
-								value={selectedQuality ?? "loading"}
-								onChange={handleQualitySelectionChange}
-							>
-								{movieQuality && movieQuality.map((quality, index) =>
-									<MenuItem id={index} value={quality.value}>{quality.label}</MenuItem>)}
-							</Select>
-
-						</div>
-						<div>
-							<MaterialButton onClick={handleWatchMovieClick}>
-								Watch Movie
-							</MaterialButton>
-							<MaterialButton
-								onClick={handleDownloadMovieClick}
-								style={{ marginLeft: "1%" }}
-							>
-								Download Movie
-							</MaterialButton>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="MovieScreenContainer">
+      <Link to="/" className="MovieScreenBackIconContainer">
+        <MdOutlineArrowBackIos className="MovieScreenBackIcon" />
+      </Link>
+      <div className="MovieScreenContentContainer">
+        <div className="MovieScreenMovieCoverContainer">
+          {coverLoading && (
+            <SkeletonTheme
+              className="MovieScreenMovieCover"
+              highlightColor="#444"
+              color="#202020"
+            >
+              <Skeleton
+                width={300}
+                height={300}
+                className="MovieScreenMovieCover"
+              />
+            </SkeletonTheme>
+          )}
+          <img
+            className="MovieScreenMovieCover"
+            style={coverLoading ? { display: "none" } : {}}
+            onLoad={handleCoverLoaded}
+            src={movieDetails && movieDetails.large_cover_image}
+          />
+        </div>
+        <div className="MovieScreenMovieDetailsContainer">
+          <div>
+            <p className="MovieScreenTitle">
+              {movieDetails && movieDetails.title_long}
+            </p>
+            <p>{movieDetails && movieDetails.genres.join(" - ")}</p>
+            <p>{movieDetails && movieDetails.description_full}</p>
+          </div>
+          <div style={{}}>
+            <div
+              style={{
+                width: "120px",
+                paddingBottom: "1%",
+              }}
+            >
+              <Select
+                className="QualitySelect"
+                id="quality"
+                value={selectedQuality ?? "loading"}
+                onChange={handleQualitySelectionChange}
+              >
+                {movieQuality &&
+                  movieQuality.map((quality, index) => (
+                    <MenuItem id={index} value={quality.value}>
+                      {quality.label}
+                    </MenuItem>
+                  ))}
+              </Select>
+            </div>
+            <div className="MovieButtons">
+              <MaterialButton onClick={handleWatchMovieClick}>
+                Watch Movie
+              </MaterialButton>
+              <Select
+                className="WatchMethodSelect"
+                style={{ color: "white" }}
+                id="watchMethod"
+                value={
+                  selectedWatchMethod ? selectedWatchMethod.value : "loading"
+                }
+                onChange={handleSelectWatchMethod}
+              >
+                {watchMethodOptions &&
+                  watchMethodOptions.map((method, index) => (
+                    <MenuItem id={index} value={method.value}>
+                      {method.label}
+                    </MenuItem>
+                  ))}
+              </Select>
+              <MaterialButton
+                onClick={handleDownloadMovieClick}
+                style={{ marginLeft: "1%" }}
+              >
+                Download Movie
+              </MaterialButton>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const mapDispatchToProps = (dispatch) => ({
-	startNewTorrent: (torrentName, magnetURI, infoHash, path) =>
-		dispatch(startNewTorrent(torrentName, magnetURI, infoHash, path)),
-	addMessage: (message, type) => dispatch(addMessage(message, type)),
+  startNewTorrent: (torrentName, magnetURI, infoHash, path) =>
+    dispatch(startNewTorrent(torrentName, magnetURI, infoHash, path)),
+  addMessage: (message, type) => dispatch(addMessage(message, type)),
 });
 
 const mapStateToProps = (state) => ({
-	torrents: state.torrents,
+  torrents: state.torrents,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(MovieScreen);
